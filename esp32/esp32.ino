6b46aae26139c9d3df206700c653903882fef738
Carolina Pimentel <carolinapimentel21@proton.me>
2023-11-06 15:23:21 -0300

    Fixing the values recieved from ads

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,72 +1,73 @@
-#include <Adafruit_ADS1X15.h>
-#include <Adafruit_I2CDevice.h>
+#include "ADS1X15.h"
 
-Adafruit_ADS1115 ads;
+int16_t infra[4];
 
-
-float infra[4];
-
-/*#define in1 9
+#define in1 9
 #define in2 8
 #define in3 11
 #define in4 10
 #define mot_l 5
 #define mot_r 6
-*/
 
+int x = 10000;
 
 float vel_l = 0x80;
 float vel_r = 0x69;
 
+ADS1115 ADS(0x48);
 
 
 void setup(void){
   
-  Serial.beging(9600);
+  Serial.begin(115200);
+  
 
-  pinMode(infra_1,INPUT);
-  pinMode(infra_2,INPUT);
-  pinMode(infra_3,INPUT);
-  pinMode(infra_4,INPUT);
 
-  if(!ads.begin())
-  {
-    Serial.println("Failed to initialize");
-     while (1);
-  }
+  ADS.begin();
   // Wait for connection
   
 }
 
 void loop(void){
 
-  infra[0] = ads.readADC_SingleEnnded(0);
-  infra[1] = ads.readADC_SingleEnnded(1);
-  infra[2] = ads.readADC_SingleEnnded(2);
-  infra[3] = ads.readADC_SingleEnnded(3);
+  infra[0] = ADS.readADC(0);
+  infra[1] = ADS.readADC(1);
+  infra[2] = ADS.readADC(2);
+  infra[3] = ADS.readADC(3);
+  /*
+  Serial.print(infra[0] );
+  Serial.print("\t");
+  Serial.print(infra[1] );
+  Serial.print("\t");
+  Serial.print(infra[2] );
+  Serial.print("\t");
+  Serial.print(infra[3] );
+  Serial.println("\t");
+  */
 
 
-  if (infra[3] == 0 && infra[2] == 1 &&  infra[1] == 0 &&infra[0] == 0  )
+
+  if (infra[3] < x && infra[2] > x &&  infra[1] < x &&infra[0] < x  )
   {
     Serial.println("curva leve esquerda");
     andar(0.55, 0.75);
   }
-  else if ( infra[3] == 0 && infra[2] == 0 && infra[1] == 1 && infra[0] == 0 )
+  else if ( infra[3] < x && infra[2] < x && infra[1] > x && infra[0] < x )
   {
      Serial.println("curva leve direita");
     andar(0.75, 0.55);
   }
-  else if ( infra[3] == 1 && infra[2] == 1 && infra[1] == 0 && infra[0] == 0)
+  else if ( infra[3] > x && infra[2] > x && infra[1] < x && infra[0] < x)
   {
      Serial.println("curva esquerda");
     andar(0.35, 0.8);
   }
-  else if ( infra[3] == 0 && infra[2] == 0 && infra[1] == 1 && infra[0] == 1)
+  else if ( infra[3] < x && infra[2] < x && infra[1] > x && infra[0] > x)
   {
      Serial.println("curva direita");
     andar(0.8 , 0.35);
   }
-  else if (infra[3] == 0 && infra[2] == 0 && infra[1] == 0 && infra[0] == 0 )
+  else if (infra[3] < x && infra[2] < x && infra[1] < x && infra[0] < x )
   {
     Serial.println("reto");
     andar(0.8, 0.8);
